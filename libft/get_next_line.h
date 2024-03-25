fd8d75e7dde76574f2d8178fd35e72461f1ff0d8
Sepahsalar <alirezasohrabizadeh@gmail.com>
2024-03-25 14:12:57 +0200

    Updated libft

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/24 16:24:41 by asohrabi          #+#    #+#             */
-/*   Updated: 2024/03/22 10:35:09 by asohrabi         ###   ########.fr       */
+/*   Created: 2023/11/03 13:36:23 by asohrabi          #+#    #+#             */
+/*   Updated: 2024/01/30 11:00:41 by asohrabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(const char *s, int c)
-{
-	if (!s)
-		return (0);
-	while (*s != '\0')
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if ((char)c == *s)
-		return ((char *)s);
-	return (0);
-}
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000
+# endif
+
+char	*get_next_line(int fd);
+int		my_strlen(char *s);
+char	*my_strjoin(char *s1, char *s2);
+char	*my_strchr(char *s, int c);
+char	*my_strdup(char *src);
+void	*ft_free_str(char *str_to_free);
+
+#endif
